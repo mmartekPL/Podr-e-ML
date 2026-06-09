c147840201f294b2379db0cdc3bf8c0139ab0879
mmartekPL <mmartek.pl@gmail.com>
2026-06-09 20:58:41 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("Mapa Europy – start aplikacji");
+
+const loginBtn = document.getElementById("loginBtn");
+const logoutBtn = document.getElementById("logoutBtn");
+const userInfo = document.getElementById("userInfo");
+
+loginBtn.addEventListener("click", () => {
+  alert("Logowanie Google podłączymy w następnym kroku.");
+});
+
+logoutBtn.addEventListener("click", () => {
+  alert("Wylogowanie podłączymy w następnym kroku.");
+});
